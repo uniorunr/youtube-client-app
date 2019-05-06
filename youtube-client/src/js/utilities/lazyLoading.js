778e32a046a60ec413470e0a4caf7641dcbe738e
Yury Chuchvaha <gagael23@live.com>
2019-05-06 16:22:00 +0300

    refactor: move lazyLoading function to separate file

---
+++ youtube-client/src/js/utilities/lazyLoading.js
@@ -0,0 +1,25 @@
+import { fetchVideoData, fetchVideoViewCount } from './requestHandler';
+import config from '../config';
+import sliderComponent from '../components/Slider/Slider';
+
+const lazyLoading = async () => {
+  const root = document.documentElement;
+  const chunk = +sessionStorage.getItem('chunk');
+  const items = +getComputedStyle(root).getPropertyValue('--items');
+  const itemsViewed = chunk * items + items;
+  const pageToken = sessionStorage.getItem('pageToken');
+  const query = sessionStorage.getItem('query');
+  const allItems = document.querySelector('.slider').children.length;
+
+  if (itemsViewed + items > allItems) {
+    const videoData = await fetchVideoData(query, config, pageToken);
+    const videoDataWithViewCount = await fetchVideoViewCount(videoData, config);
+
+    if (videoDataWithViewCount) {
+      const lastIndex = document.querySelector('.slider').children.length;
+      sliderComponent(videoDataWithViewCount, 'load', lastIndex);
+    }
+  }
+};
+
+export default lazyLoading;
--- youtube-client/src/js/utilities/sliderListener.js
+++ youtube-client/src/js/utilities/sliderListener.js
@@ -1,25 +1,4 @@
-import config from '../config';
-import { fetchVideoData, fetchVideoViewCount } from './requestHandler';
-import sliderComponent from '../components/Slider/Slider';
-
-const lazyLoading = async () => {
-  const root = document.documentElement;
-  const chunk = +sessionStorage.getItem('chunk');
-  const items = +getComputedStyle(root).getPropertyValue('--items');
-  const itemsViewed = chunk * items + items;
-  const pageToken = sessionStorage.getItem('pageToken');
-  const query = sessionStorage.getItem('query');
-  const allItems = document.querySelector('.slider').children.length;
-
-  if (itemsViewed + items > allItems) {
-    const videoData = await fetchVideoData(query, config, pageToken);
-    const videoDataWithViewCount = await fetchVideoViewCount(videoData, config);
-
-    if (videoDataWithViewCount) {
-      sliderComponent(videoDataWithViewCount, 'load', allItems);
-    }
-  }
-};
+import lazyLoading from './lazyLoading';
 
 const sliderListener = (slider) => {
   if (!slider.dataset.listener) {
