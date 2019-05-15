30e9ab746f49bd571f07e062a40b73aa9c4d5806
Yury Chuchvaha <gagael23@live.com>
2019-05-16 00:50:26 +0300

    fix: fix public path in webpack config

--- youtube-client/webpack.config.js
+++ youtube-client/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.bundle.js',
-    publicPath: 'dist/',
+    publicPath: '/',
   },
   module: {
     rules: [{
