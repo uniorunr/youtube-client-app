42c06cd1d54bc57494ffe044463363f174d6b78a
Yury Chuchvaha <gagael23@live.com>
2019-05-18 23:01:25 +0300

    test: add tests for SliderItem component

---
+++ youtube-client/src/components/Slider/SliderItem/SliderItem.test.js
@@ -0,0 +1,31 @@
+import sliderItemComponent from './SliderItem';
+
+describe('sliderItemComponent', () => {
+  it('should be instance of Function', () => {
+    expect(sliderItemComponent).toBeInstanceOf(Function);
+  });
+
+  it('should be rendered correctly', () => {
+    const data = {
+      testVideoId1: {
+        channelTitle: 'channelTitle1',
+        description: 'description1',
+        publishedAt: 'publishedAt1',
+        thumbnailUrl: 'thumbnailUrl1',
+        title: 'title1',
+        viewCount: 'viewCount1',
+      },
+      testVideoId2: {
+        channelTitle: 'channelTitle2',
+        description: 'description2',
+        publishedAt: 'publishedAt2',
+        thumbnailUrl: 'thumbnailUrl2',
+        title: 'title2',
+        viewCount: 'viewCount2',
+      },
+    };
+    sliderItemComponent('body', data);
+    expect(document.body.innerHTML).toMatchSnapshot();
+    document.body.innerHTML = '';
+  });
+});
